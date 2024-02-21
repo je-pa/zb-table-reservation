130d4ed26fa5bd3f696893a17fc7c95d31e78b54
je-pa <jieun0502@gmail.com>
2024-02-22 02:45:57 +0900

    init commit

---
+++ src/main/java/com/zb/tablereservation/ZbTableReservationApplication.java
@@ -0,0 +1,13 @@
+package com.zb.tablereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbTableReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbTableReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zb/tablereservation/ZbTableReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zb.tablereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbTableReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
