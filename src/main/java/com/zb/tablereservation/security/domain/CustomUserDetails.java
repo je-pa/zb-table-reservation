7855f501702c183ef74b1e7e63ae9713b64ab25f
je-pa <jieun0502@gmail.com>
2024-03-02 15:58:38 +0900

    UserDetails

---
+++ src/main/java/com/zb/tablereservation/security/domain/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package com.zb.tablereservation.security.domain;
+
+import com.zb.tablereservation.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> list = new ArrayList<>();
+        list.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return list;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
