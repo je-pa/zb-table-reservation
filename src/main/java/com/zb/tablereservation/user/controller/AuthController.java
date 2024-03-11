7b1a1d604b3f2132ae5284006bd88d071f691665
je-pa <jieun0502@gmail.com>
2024-03-11 20:07:03 +0900

    user 설명 추가

--- src/main/java/com/zb/tablereservation/user/controller/AuthController.java
+++ src/main/java/com/zb/tablereservation/user/controller/AuthController.java
@@ -16,7 +16,7 @@ public class AuthController {
 
     /**
      * 회원가입을 한다.
-     *
+     * 비밀번호는 시큐리티에서 제공하는 암호화를 사용해서 삽입한다.
      * @param request request의 role 값에 의해 일반 유저 또는 파트너 가입을 할 수 있다.
      * @return 회원가입이 정상 완료 되었다면 회원가입이 완료된 유저 정보를 Body에 담아 리턴한다.
      */
@@ -25,6 +25,11 @@ public class AuthController {
         return ResponseEntity.ok(userService.signup(request));
     }
 
+    /**
+     * 로그인을 한다.
+     * @param request 로그인 정보를 받는다.
+     * @return 토큰값을 리턴한다.
+     */
     @PostMapping("/signin")
     public ResponseEntity<Signin.Response> signin(@Valid @RequestBody Signin.Request request){
         return ResponseEntity.ok(userService.signin(request));
